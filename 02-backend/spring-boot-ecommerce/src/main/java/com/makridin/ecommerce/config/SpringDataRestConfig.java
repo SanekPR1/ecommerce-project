0c1c53f9496337a5047bac00b0a57023278ad50f
Aleksandr Makridin <sanekpr1@gmail.com>
2022-02-13 16:29:00 +0100

    007: server: added spring config to prohibit delete put and post methods

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/makridin/ecommerce/config/SpringDataRestConfig.java
@@ -0,0 +1,29 @@
+package com.makridin.ecommerce.config;
+
+import com.makridin.ecommerce.entity.Product;
+import com.makridin.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class SpringDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] unsupportedActions = {HttpMethod.DELETE, HttpMethod.POST, HttpMethod.PUT};
+
+        //disable HTTP methods for Product and ProductCategory: POST, PUT, DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+    }
+}
