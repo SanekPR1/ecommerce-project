80be0ec9741063cbb03625417fc7a387e2982377
Aleksandr Makridin <sanekpr1@gmail.com>
2022-03-05 14:08:47 +0100

    063: front: injected cart service into product-list.component + implemented addToCart method

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -22,6 +24,7 @@ export class ProductListComponent implements OnInit {
 
   constructor(
     private productService: ProductService,
+    private cartService: CartService,
     private route: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -48,7 +51,8 @@ export class ProductListComponent implements OnInit {
   addToCart(product: Product) {
     console.log(`Adding to cart: ${product.name}, ${product.unitPrice}`);
 
-    //TODO: implement logic
+    const cartItem = new CartItem(product);
+    this.cartService.addToCart(cartItem);
   }
 
   private handleSearchProducts() {
